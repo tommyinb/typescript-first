c24692dc6cbf263e9ca23ed9f2f57ce57370e281
Tommy <tommy@tom.my>
2022-10-06 10:20:46 +0800

    start message

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Abc } from "./abc.js";
 
 const abc: Abc = {
-  a: "abc",
+  a: "start with TypeScript",
   b: 123,
 };
 
-console.log(`${abc.a}${abc.b}`);
+console.log(`${abc.a} ${abc.b}`);
